02bdee6d87b4cd0a51860a399ba104e3c58055a3
JuanOtavaloMaigua <juanotavalomaigua@gmail.com>
2021-01-12 22:42:57 -0500

    solucion de errores

--- app.js
+++ app.js
@@ -1,24 +1,23 @@
 
 var slideIndex = 0
 var slides = document.getElementsByClassName("mySlide")
-var dots = document.getElementsByClassName("dot")
-var globalJump = 0
+var dots = document.getElementsByClassName("do_list")
+console.log(dots)
 
-function plusSlide(jump){
-    globalJump = jump
-}
-
+console.log(slides)
 function automaticAnimation(){
     for(i=0; i< slides.length; i++){
         slides[i].style.display = "none"
-        dots[i].style.backgroundColor = "#FF0000"
     }
     slides[slideIndex].style.display = "block"
-    dots[slideIndex].style.backgroundColor = "#0000FF"
     slideIndex++
     if(slideIndex >= (slides.length)){ slideIndex = 0}
 
-    setTimeout(automaticAnimation, 5000)    
+    setTimeout(automaticAnimation, 3000)    
+}
+
+function plusSlide(jump){
+
 }
 
 automaticAnimation()
