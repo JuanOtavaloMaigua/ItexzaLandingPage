8a7361d3e8e5922afa37b6ff21525ccf1755c317
JuanOtavaloMaigua <juanotavalomaigua@gmail.com>
2021-01-13 11:01:51 -0500

    se termino el message company

--- app.js
+++ app.js
@@ -1,23 +1,28 @@
 
 var slideIndex = 0
 var slides = document.getElementsByClassName("mySlide")
-var dots = document.getElementsByClassName("do_list")
-console.log(dots)
+var dots = document.getElementsByClassName("dot")
+var slidesSomos = document.getElementsByClassName("message_company_messages_item")
 
-console.log(slides)
+console.log("slideSomos: ",slidesSomos)
+console.log(slidesSomos)
+
 function automaticAnimation(){
     for(i=0; i< slides.length; i++){
         slides[i].style.display = "none"
+        dots[i].style.backgroundColor = "#1458AA"
+        slidesSomos[i].style.display = "none"
     }
     slides[slideIndex].style.display = "block"
+    dots[slideIndex].style.backgroundColor = "#F50006"
+    slidesSomos[slideIndex].style.display = "block"
     slideIndex++
     if(slideIndex >= (slides.length)){ slideIndex = 0}
 
     setTimeout(automaticAnimation, 3000)    
 }
 
-function plusSlide(jump){
 
-}
+
 
 automaticAnimation()
