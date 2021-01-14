86cc6d6010f164204eb3bb4b2a61b42a9840abd3
JuanOtavaloMaigua <juanotavalomaigua@gmail.com>
2021-01-13 22:53:00 -0500

    se ha terminado el homepage

--- app.js
+++ app.js
@@ -23,6 +23,23 @@ function automaticAnimation(){
 }
 
 
+// Initialize and add the map
+function initMap() {
+    // The location of Uluru
+    // 0.231701, -78.259124
+    const uluru = { lat: 0.231701, lng:-78.259124};
+    // The map, centered at Uluru
+    const map = new google.maps.Map(document.getElementById("map"), {
+      zoom: 15,
+      center: uluru,
+    });
+    // The marker, positioned at Uluru
+    const marker = new google.maps.Marker({
+      position: uluru,
+      map: map,
+    });
+  }
 
 
 automaticAnimation()
+initMap()
